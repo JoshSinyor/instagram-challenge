1c490aff7ba40210c1acc2695b4660959bb7eb3f
Joshua Sinyor <>
2021-02-28 22:40:48 +0000

    Update user.rb and schema.rb. Reflects updating of Users table structure.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
+# The User model instantiates individual users.
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true
 end
--- db/migrate/20210228221448_create_users.rb
+++ db/migrate/20210228221448_create_users.rb
@@ -1,9 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :email
+      t.string :email, null: false
       t.string :password_digest
-      t.string :string
+      t.string :username, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :pfp_url
 
       t.timestamps
     end
