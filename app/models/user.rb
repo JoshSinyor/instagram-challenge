ea38bfcad5180e209a2e7c61eace7bb1c0e13c92
Joshua Sinyor <>
2021-02-28 23:11:21 +0000

    Update models/user.rb. Reflects validation of input.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "E-mail address invalid." }
-  validates :email, uniqueness: { true, message: "E-mail address already in use." }
+  validates :email, uniqueness: { case_sensitive: false, message: "E-mail address already in use." }
+  validates :username, uniqueness: { case_sensitive: false, message: "Username already in use." }
 end
