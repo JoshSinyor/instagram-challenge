9d99e3ec046a9244a2a25ea634aa2dfa966bb4d9
Joshua Sinyor <>
2021-03-01 17:33:54 +0000

    Update seeds.rb. Reflects refactoring.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,42 +6,25 @@
 # movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 # Character.create(name: 'Luke', movie: movies.first)
 
+# Users
+
 User.create({:email => "adam.anderson@makers.tech", :password => "password2", :password_confirmation => "password2", :first_name => "Adam", :last_name => "Anderson", :username => "gardenofadam", :pfp_url => "anderson_adam.jpg"})
-
 User.create({:email => "bailey.brown@makers.tech", :password => "123456789", :password_confirmation => "123456789", :first_name => "Bailey", :last_name => "Brown", :username => "deniedbailey", :pfp_url => "brown_bailey.jpg"})
-
 User.create({:email => "carol.clark@makers.tech", :password => "qwerty12345", :password_confirmation => "qwerty12345", :first_name => "Carol", :last_name => "Clark", :username => "clarkcent", :pfp_url => "clark_carol.jpg"})
-
 User.create({:email => "duncan.davis@makers.tech", :password => "hunter2bash", :password_confirmation => "hunter2bash", :first_name => "Duncan", :last_name => "Davis", :username => "duncandisorderly", :pfp_url => "davis_duncan.jpg"})
-
 User.create({:email => "ellen.ellis@makers.tech", :password => "asdfghjkl", :password_confirmation => "asdfghjkl", :first_name => "Ellen", :last_name => "Ellis", :username => "ellsbells", :pfp_url => "ellis_ellen.jpg"})
-
 User.create({:email => "gordon.freeman@makers.tech", :password => "blackmesa1", :password_confirmation => "blackmesa1", :first_name => "Gordon", :last_name => "Freeman", :username => "noplsgman", :pfp_url => "freeman_gordon.jpg"})
-
 User.create({:email => "hannah.hamilton@makers.tech", :password => "picture1", :password_confirmation => "picture1", :first_name => "Hannah", :last_name => "Hamilton", :username => "hanham", :pfp_url => "hamilton_hannah.jpg"})
-
 User.create({:email => "ian.ibsen@makers.tech", :password => "password", :password_confirmation => "password", :first_name => "Ian", :last_name => "Ibsen", :username => "dibsonibs", :pfp_url => "ibsen_ian.jpg"})
-
 User.create({:email => "joshua.jones@makers.tech", :password => "senha1995", :password_confirmation => "senha1995", :first_name => "Joshua", :last_name => "Jones", :username => "jayjay", :pfp_url => "jones_joshua.jpg"})
-
 User.create({:email => "kelly.kennedy@makers.tech", :password => "abc123def456", :password_confirmation => "abc123def456", :first_name => "Kelly", :last_name => "Kennedy", :username => "badluckgirl", :pfp_url => "kennedy_kelly.jpg"})
-
 User.create({:email => "liam.lewis@makers.tech", :password => "million20", :password_confirmation => "million20", :first_name => "Liam", :last_name => "Lewis", :username => "bigl", :pfp_url => "lewis_liam.jpg"})
-
 User.create({:email => "mavis.miller@makers.tech", :password => "0000000000", :password_confirmation => "0000000000", :first_name => "Mavis", :last_name => "Miller", :username => "marvelatmavis", :pfp_url => "miller_mavis.jpg"})
-
 User.create({:email => "nelson.newman@makers.tech", :password => "iloveyou", :password_confirmation => "iloveyou", :first_name => "Nelson", :last_name => "Newman", :username => "haa-haa", :pfp_url => "newman_nelson.jpg"})
-
 User.create({:email => "oliver.owens@makers.tech", :password => "aaron431", :password_confirmation => "aaron431", :first_name => "Oliver", :last_name => "Owens", :username => "olio", :pfp_url => "owens_oliver.jpg"})
-
 User.create({:email => "pamela.powell@makers.tech", :password => "covfefe45", :password_confirmation => "covfefe45", :first_name => "Pamela", :last_name => "Powell", :username => "pampampam", :pfp_url => "powell_pamela.jpg"})
-
 User.create({:email => "queenie.quartermaine@makers.tech", :password => "password1", :password_confirmation => "password1", :first_name => "Queenie", :last_name => "Quartermaine", :username => "q_q", :pfp_url => "quartermaine_queenie.jpg"})
-
 User.create({:email => "robert.rogers@makers.tech", :password => "qqww1122", :password_confirmation => "qqww1122", :first_name => "Robert", :last_name => "Rogers", :username => "thecabinboy", :pfp_url => "roger_roberts.jpg"})
-
 User.create({:email => "sarah.smith@makers.tech", :password => "pokemoncatchemall", :password_confirmation => "pokemoncatchemall", :first_name => "Sarah", :last_name => "Smith", :username => "sasparilla", :pfp_url => "smith_sarah.jpg"})
-
 User.create({:email => "taylor.thompson@makers.tech", :password => "blink182", :password_confirmation => "blink182", :first_name => "Taylor", :last_name => "Thompson", :username => "callmet", :pfp_url => "thompson_taylor.jpg"})
-
 User.create({:email => "ulysses.upright@makers.tech", :password => "batman&robin", :password_confirmation => "batman&robin", :first_name => "Ulysses", :last_name => "Upright", :username => "fitemirl", :pfp_url => "upright_ulysses.jpg"})
