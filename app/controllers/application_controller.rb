72c3c675b15d95d4f5934a13439ffa91335ba734
Joshua Sinyor <>
2021-03-03 16:50:07 +0000

    Update about_controller.rb, application_controller.rb and register_controller.rb. Reflects limited access.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,21 @@
 
 # The ApplicationController handles overarching requests for all other controllers.
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :set_current_user
+
+  private
 
   def set_current_user
     Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
   end
+
+  def require_current_user
+    redirect_to root_path, alert: "Please log in first." unless Current.user
+  end
+
+  def require_no_current_user
+    redirect_to root_path, alert: "Please log out first." if Current.user
+  end
+
 end
